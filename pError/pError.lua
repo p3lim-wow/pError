6be2562f1c40f7a0398bd7e4da7d0c00ffd4b08e
p3lim-52096 <p3lim-52096@7a612bb6-1b7b-4f5f-b58b-e1cc54d1b6b6>
2008-07-28 06:33:34 +0000

    Removed whitelist, no need for it
    git-svn-id: svn://svn.wowinterface.com/pError-161/trunk@4 7a612bb6-1b7b-4f5f-b58b-e1cc54d1b6b6

--- pError/pError.lua
+++ pError/pError.lua
@@ -1,23 +1,23 @@
 --[[
 	You can find more events to add to the "blacklist" from this list:
-	http://wowcompares.com/0438606/FrameXML/GlobalStrings.lua
+	http://www.wowwiki.com/WoW_Constants/Errors
 	
 	Just use ctrl+f (the find function)
 --]]
 
 local blacklist = {
-	SPELL_FAILED_NO_COMBO_POINTS,   -- That ability requires combo points
-	SPELL_FAILED_TARGETS_DEAD,      -- Your target is dead
-	SPELL_FAILED_SPELL_IN_PROGRESS, -- Another action is in progress
-	SPELL_FAILED_TARGET_AURASTATE,  -- You can't do that yet. (TargetAura)
-	SPELL_FAILED_CASTER_AURASTATE,  -- You can't do that yet. (CasterAura)
-	SPELL_FAILED_NO_ENDURANCE,      -- Not enough endurance
-	SPELL_FAILED_BAD_TARGETS,       -- Invalid target
-	SPELL_FAILED_NOT_MOUNTED,       -- You are mounted
-	SPELL_FAILED_NOT_ON_TAXI,       -- You are in flight
-	SPELL_FAILED_NOT_INFRONT,       -- You must be in front of your target
-	SPELL_FAILED_NOT_IN_CONTROL,    -- You are not in control of your actions
-	SPELL_FAILED_MOVING,            -- Can't do that while moving
+	ERR_SPELL_FAILED_NO_COMBO_POINTS,   -- That ability requires combo points
+	ERR_SPELL_FAILED_TARGETS_DEAD,      -- Your target is dead
+	ERR_SPELL_FAILED_SPELL_IN_PROGRESS, -- Another action is in progress
+	ERR_SPELL_FAILED_TARGET_AURASTATE,  -- You can't do that yet. (TargetAura)
+	ERR_SPELL_FAILED_CASTER_AURASTATE,  -- You can't do that yet. (CasterAura)
+	ERR_SPELL_FAILED_NO_ENDURANCE,      -- Not enough endurance
+	ERR_SPELL_FAILED_BAD_TARGETS,       -- Invalid target
+	ERR_SPELL_FAILED_NOT_MOUNTED,       -- You are mounted
+	ERR_SPELL_FAILED_NOT_ON_TAXI,       -- You are in flight
+	ERR_SPELL_FAILED_NOT_INFRONT,       -- You must be in front of your target
+	ERR_SPELL_FAILED_NOT_IN_CONTROL,    -- You are not in control of your actions
+	ERR_SPELL_FAILED_MOVING,            -- Can't do that while moving
 	ERR_ATTACK_FLEEING,				-- Can't attack while fleeing.
 	ERR_ITEM_COOLDOWN,				-- Item is not ready yet.
 	ERR_GENERIC_NO_TARGET,          -- You have no target.
@@ -29,11 +29,6 @@ local blacklist = {
 	ERR_INVALID_ATTACK_TARGET,      -- You cannot attack that target.
 	ERR_OUT_OF_MANA,                -- Not enough mana
 	ERR_NOEMOTEWHILERUNNING,        -- You can't do that while moving!
-	OUT_OF_ENERGY,                  -- Not enough energy.
-}
-
-local whitelist = {
-	ERR_BADATTACKFACING,			-- You are facing the wrong way!
 }
 
 local lastEvent
@@ -42,8 +37,6 @@ function UIErrorsFrame_OnEvent(event, msg, ...)
 	for _,text in pairs(blacklist) do
 		if(text and msg and msg == text) then return end
 	end
-	for _,text in pairs(whitelist) do
-		if(msg and msg == lastEvent and msg ~= text) then return end
-	end
+	if(msg and msg == lastEvent) then return end
 	return oldUIErrorsFrame_OnEvent(event, msg, ...)
 end
